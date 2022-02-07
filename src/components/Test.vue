57017d622364ff4ac97e6e88723f8ce99a85b5a7
Cindy Cheng <71624875+Cindy6166@users.noreply.github.com>
2022-02-07 08:36:23 +0800

    [practice] usage of transition

---
+++ src/components/Test.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+      <button @click="isShow=!isShow">Show/Hide</button>
+      <transition name="hello" appear>
+        <h1 v-show="isShow" class="come">Hello!</h1>
+      </transition>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'Test',
+    data(){
+        return {
+            isShow:true
+        }
+    },
+
+}
+</script>
+
+<style>
+    h1{
+        background-color: coral;
+    }
+    .hello-enter-active{
+        animation: hi 0.5s linear;
+    }
+    .hello-leave-active{
+        animation: hi 0.5s linear reverse;
+    }
+
+    @keyframes hi {
+        from{
+            transform: translateX(-100%);
+        }
+        to{
+           transform: translate(0px); 
+        }
+    }
+</style>
---
+++ src/components/Test2.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+      <button @click="isShow=!isShow">Show/Hide</button>
+      <transition-group name="hello" appear>
+        <h1 v-show="!isShow" key="1">Hello!</h1>
+        <h1 v-show="isShow" key="2">Hi!</h1>
+      </transition-group>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'Test',
+    data(){
+        return {
+            isShow:true
+        }
+    },
+
+}
+</script>
+
+<style>
+    h1{
+        background-color: coral;
+    }
+    .hello-enter, .hello-leave-to{
+        transform: translateX(-100%);
+    }
+    .hello-enter-active, .hello-leave-active{
+        transition: 0.5s linear;
+    }
+    .hello-enter-to, .hello-leave {
+        transform: translateX(0);
+    }
+  
+   
+</style>
