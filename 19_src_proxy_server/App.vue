304d79f2a061c50280112aef9f0ccaa8f0cc789b
Cindy Cheng <71624875+Cindy6166@users.noreply.github.com>
2022-02-07 15:39:04 +0800

    config: config proxy server

---
+++ 19_src_proxy_server/App.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <button @click="getStudents">get students</button>
+        <button @click="getCars">get cars</button>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+   name:'App',
+   methods: {
+       getStudents(){
+           axios.get('http://localhost:8080/myApi/students').then(
+               response => {
+                   console.log('success', response.data);
+               },
+               error => {
+                   console.log('error',error.message);
+               }
+           )
+       },
+       getCars(){
+           axios.get('http://localhost:8080/api/cars').then(
+               response => {
+                   console.log('success', response.data);
+               },
+               error => {
+                   console.log('error',error.message);
+               }
+           )
+       }
+   }
+}
+</script>
--- vue.config.js
+++ vue.config.js
@@ -6,5 +6,27 @@ module.exports = {
       },
     },
     //off the lint
-    lintOnSave:false
+    lintOnSave:false,
+    // open proxy (way 1)
+    /* devServer: {
+      proxy: 'http://localhost:5000'
+    } */
+
+    //open proxy (way 2)
+    devServer: {
+      proxy: {
+        '^/myApi': {
+          target: 'http://localhost:5000',
+          pathRewrite:{'^/myApi':''},
+          ws: true, // websocket
+          changeOrigin: true
+        },
+        '^/api': {
+          target: 'http://localhost:5001',
+          pathRewrite:{'^/api':''},
+          ws: true, // websocket
+          changeOrigin: true
+        }
+      }
+    }
   }
