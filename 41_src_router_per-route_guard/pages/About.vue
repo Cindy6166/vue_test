93abc971911642bcd925247560ad5b84a89065f3
Cindy Cheng <71624875+Cindy6166@users.noreply.github.com>
2022-02-11 05:15:01 +0800

    feat: usage of per-route guards

---
+++ 41_src_router_per-route_guard/pages/About.vue
@@ -0,0 +1,18 @@
+<template>
+    <h2>我是About的内容</h2>
+</template>
+
+<script>
+export default {
+  name: "About",
+  // mounted(){
+  //   console.log('mounted');
+  // },
+  // beforeDestroy(){
+  //   console.log('destroy');
+  // }
+};
+</script>
+
+<style>
+</style>
---
+++ 41_src_router_per-route_guard/pages/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h2>Home组件内容</h2>
+    <div>
+      <ul class="nav nav-tabs">
+        <li>
+          <router-link class="list-group-item" active-class="active" to="/home/news">News</router-link>
+        </li>
+        <li>
+          <router-link class="list-group-item" active-class="active" to="/home/message">Message</router-link>
+        </li>
+      </ul>
+      <!-- cashe some route components -->
+      <!-- <keep-alive :include="['News', 'Message']"> -->
+      <!-- cache a route component: config component name, make it stop to destroy -->
+      <keep-alive include="News"> 
+        <router-view></router-view>
+      </keep-alive>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  // mounted(){
+  //   console.log(this);
+  //   window.homeRoute = this.$route
+  //   window.homeRouter = this.$router
+  // }
+};
+</script>
+
+<style>
+</style>
