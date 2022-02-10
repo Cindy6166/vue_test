f6a390acc909b1bc3f89997b72679e62de7dbdf3
Cindy Cheng <71624875+Cindy6166@users.noreply.github.com>
2022-02-10 23:09:24 +0800

    feat: usage of keep-alive and hook(activated, deactivated)

---
+++ 39_src_router_lifecycle_hook/pages/Detail.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul>
+    <li>Number: {{id}}</li>
+    <li>Title: {{title}}</li>
+    <!-- <li>a:{{a}}, b:{{b}}</li> -->
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "Detail",
+  props:['id','title'],
+  // computed:{
+  //   id(){
+  //     return this.$route.query.id
+  //   },
+  //   title(){
+  //     return this.$route.query.title
+  //   }
+  // },
+  mounted() {
+    console.log(this.$route);
+  },
+};
+</script>
+
+<style>
+</style>
