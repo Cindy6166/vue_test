c9d0b6bf23ca0421c08fbc14f0af953c30a4df17
Cindy Cheng <71624875+Cindy6166@users.noreply.github.com>
2022-02-10 14:57:06 +0800

    feat: router nested

--- 31_src_router_nested/pages/About.vue
+++ 31_src_router_nested/pages/About.vue
@@ -1,12 +1,16 @@
 <template>
-  <div>
     <h2>我是About的内容</h2>
-  </div>
 </template>
 
 <script>
 export default {
   name: "About",
+  // mounted(){
+  //   console.log('mounted');
+  // },
+  // beforeDestroy(){
+  //   console.log('destroy');
+  // }
 };
 </script>
 
