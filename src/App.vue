ea98b8b885471ab575e9ae666833a035c05d31b6
Cindy Cheng <71624875+Cindy6166@users.noreply.github.com>
2022-02-11 07:35:17 +0800

    feat: config and use ElementUI

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,25 @@
 <template>
   <div>
-    <div class="row">
-      <Banner/>
-    </div>
-    <div class="row">
-      <div class="col-xs-2 col-xs-offset-2">
-        <div class="list-group">
-          <!-- primitive HTML for page navigation -->
-          <!-- <a class="list-group-item" href="./about.html">About</a>
-          <a class="list-group-item active" href="./home.html">Home</a> -->
-
-          <!-- In Vue use router-link to realize switch route  -->
-          <router-link replace class="list-group-item" active-class="active" :to="{name:'myAbout'}">About</router-link>
-          <router-link replace class="list-group-item" active-class="active" to="/home">Home</router-link>
-        </div>
-      </div>
-      <div class="col-xs-6">
-        <div class="panel">
-          <div class="panel-body">
-            <!-- point out the position of component  -->
-            <router-view></router-view>
-          </div>
-        </div>
-      </div>
-    </div>
+    <button>original button</button>
+    <input type="text" />
+    <el-row>
+      <my-button>默认按钮</my-button>
+      <my-button type="primary">主要按钮</my-button>
+      <my-button type="success">成功按钮</my-button>
+      <my-button type="info">信息按钮</my-button>
+      <my-button type="warning">警告按钮</my-button>
+      <my-button type="danger">危险按钮</my-button>
+    </el-row>
+     <el-date-picker
+      v-model="value1"
+      type="date"
+      placeholder="选择日期">
+    </el-date-picker>
   </div>
 </template>
 
 <script>
-import Banner from './components/Banner.vue'
 export default {
   name: "App",
-  components: {Banner},
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,16 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
-//import VueRouter
-import VueRouter from 'vue-router'
-//import router
-import router from './router'
+
+// wholly import
+// import ElementUI
+// import ElementUI from 'element-ui';
+// import whole css style
+// import 'element-ui/lib/theme-chalk/index.css';
+
+//partly import
+import { Button,Row,DatePicker } from 'element-ui'
 
 Vue.config.productionTip = false
 
-Vue.use(VueRouter)
+// apply
+// Vue.use(ElementUI);
+Vue.component('my-button',Button)
+Vue.component(Row.name, Row);
+Vue.component(DatePicker.name, DatePicker);
+
 
 new Vue({
   el: '#app',
   render: h => h(App),
-  router: router
 })
